8bd3f67436f85af0ddc0aa82344437c8be45fd4f
honeda <50893852+honeda@users.noreply.github.com>
2022-03-29 22:25:42 +0900

    docs: change the comment

--- src/network/policy_value_resnet.py
+++ src/network/policy_value_resnet.py
@@ -79,6 +79,7 @@ class PolicyValueNetwork(nn.Module):
         # value head
         value = F.relu(self.value_bn1(self.value_conv1(x)))
         value = F.relu(self.value_fc1(torch.flatten(value, 1)))
-        value = self.value_fc2(value)  # sigmoidに通さない. 代わりにBCEWithLogitsLossを使う.
+        # 評価値を0~1にしないため、sigmoidに通さない. 代わりにBCEWithLogitsLossを使う.
+        value = self.value_fc2(value)
 
         return policy, value
